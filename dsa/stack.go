a09b7edb7cbedaab02228ad8e95429111b62a9f5
tuxy <subham020304@gmail.com>
2025-06-19 22:14:29 +0000

    refactor: stack.pop should return the removed element

--- dsa/stack.go
+++ dsa/stack.go
@@ -6,8 +6,10 @@ func (s *Stack[T]) size() int {
 	return len(*s)
 }
 
-func (s *Stack[T]) pop() {
+func (s *Stack[T]) pop() T {
+	lastElement := (*s)[len(*s)-1]
 	*s = (*s)[:len(*s)-1]
+	return lastElement
 }
 
 func (s *Stack[T]) push(element T) {
